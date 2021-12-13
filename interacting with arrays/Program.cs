518cdee8af934f7ecbdaa33ef180f55394a6219f
Michael Walker <walkerrepair255@yahoo.com>
2021-12-13 12:13:30 -0500

    salesItem class sample to play with

---
+++ interacting with arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TimeCode
+{
+    public class Program
+    {
+        private const string Format = "Your account balance is {0:C2} {1:C2} {0:C2}.";
+
+        static void Main()
+        {
+            var item = new SaleItem("Shoes", 19.95m);
+            Console.WriteLine($"{item.Name}: sells for {item.Price:C2}");
+
+            decimal t = 432.321m;
+            decimal value = 123.456m;
+            Console.WriteLine(Format, value, t);
+        }
+    }
+}
